7579cb25354845fe37ebf6efd6a9907533f018a3
Vivek Kashyap <vivekyeskashyap@gmail.com>
2022-10-18 08:40:24 +0530

    modulus

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ async fn main() {
         .route("/", get(handler))
         .route("/add", get(add))
         .route("/multiply", get(multiply))
-        .route("/divide", get(divide));
+        .route("/divide", get(divide))
+        .route("/modulus", get(modulus));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -32,7 +33,13 @@ async fn multiply(Query(params): Query<HashMap<String, String>>) -> Html<String>
     let product = &params["x"].parse::<i32>().unwrap() * &params["y"].parse::<i32>().unwrap();
     Html(format!("<h1>{}</h1>", product))
 }
+
 async fn divide(Query(params): Query<HashMap<String, String>>) -> Html<String> {
     let quot = &params["x"].parse::<f32>().unwrap() / &params["y"].parse::<f32>().unwrap();
     Html(format!("<h1>{}</h1>", quot))
+}
+
+async fn modulus(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+    let ans = &params["x"].parse::<f32>().unwrap() % &params["y"].parse::<f32>().unwrap();
+    Html(format!("<h1>{}</h1>", ans))
 }
